d16167e537f2f13c9d214a830d06f8436a71d1b4
DanielaCatalan93 <dcdaniela93@gmail.com>
2024-12-04 21:57:45 -0300

    se agregan comentarios

--- ordenarLista/script.js
+++ ordenarLista/script.js
@@ -1,14 +1,14 @@
 let btn = document.querySelector("button");
 let lista = document.querySelector("ul");
 
-let listaMichis = [...lista.children];
+let listaMichis = [...lista.children]; //transforma la lista en un array
 
 listaMichis.forEach((item) => {
   console.log(item.textContent);
 });
 
 btn.addEventListener("click", () => {
-  listaMichis.sort((a, b) => a.textContent.localeCompare(b.textContent));
+  listaMichis.sort((a, b) => a.textContent.localeCompare(b.textContent)); //ordena alfabeticamente el arreglo.
 
   listaMichis.forEach((michi) => lista.appendChild(michi));
 });
